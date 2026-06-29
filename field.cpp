cd8fdaf86ceb16f927061de0f02845130a9f75e2
naleta <markonaletilic4@gmail.com>
2026-06-29 17:36:24 +0200

    Add files via upload

---
+++ field.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int field[3];
+    cin >> field[0];
+cin >> field[1];    
+   field[2] = field[0] + field[1];
+    cout << field[2] << endl;
+    return 0;
+}
